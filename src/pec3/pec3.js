4f5262217d55c282af4e281dd839d1a4d994a666
alexcordonvila <alexcordonvila@gmail.com>
2024-12-18 19:39:47 +0100

    Resolver Ejercicio 1

--- src/pec3/pec3.js
+++ src/pec3/pec3.js
@@ -1,5 +1,15 @@
 // Exercise 1
 export function fetchUser(id, callback) { 
+    const users = {
+        1: "Brendan Eich",
+        2: "Douglas Crockford",
+        3: "Jeremy Ashkenas",
+    };
+    if(Number.isInteger(id) && id in users){
+            callback(users[id]);
+            return true;
+    }
+    return false;
 }
 
 // Exercise 2
