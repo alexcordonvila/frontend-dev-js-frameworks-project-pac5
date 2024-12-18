a83787dad3357f3f8d753e94a98fd64a2ece4503
alexcordonvila <alexcordonvila@gmail.com>
2024-12-18 21:38:08 +0100

    Resolver Ejercicio 2

--- src/pec3/pec3.js
+++ src/pec3/pec3.js
@@ -1,19 +1,40 @@
 // Exercise 1
-export function fetchUser(id, callback) { 
+export function fetchUser(id, callback) {
     const users = {
         1: "Brendan Eich",
         2: "Douglas Crockford",
         3: "Jeremy Ashkenas",
     };
-    if(Number.isInteger(id) && id in users){
-            callback(users[id]);
-            return true;
+    if (Number.isInteger(id) && id in users) {
+        callback(users[id]);
+        return true;
     }
     return false;
 }
 
 // Exercise 2
-export function getComments(fetchUser, fetchPosts, fetchComments) {
+export async function getComments(fetchUser, fetchPosts, fetchComments) {
+    return new Promise((resolve, reject) => {
+        fetchUser()
+            .then(userID => {
+                return fetchPosts(userID)
+                    .then(posts => {
+                        return fetchComments(posts)
+                            .then(comments => {
+                                resolve(comments);
+                            })
+                            .catch(() => {
+                                reject(new Error('Error: Comments fetch failed')); 
+                            });
+                    })
+                    .catch(() => {
+                        reject(new Error('Error: Posts fetch failed')); 
+                    });
+            })
+            .catch(() => {
+                reject(new Error('Error: User fetch failed')); 
+            });
+    });
 }
 
 // Exercise 3
